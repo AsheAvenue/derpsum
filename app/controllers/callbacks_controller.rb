bcfeb00b721c49daf81f16a07459dd1092f8abdf
Tim Boisvert <tboisvert@asheavenue.com>
2015-03-02 12:24:03 -0500

    Only allow certain user account domains to log in

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,8 +1,16 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    allowed_domains = ENV['ALLOWED_DOMAINS'].split(',')
+    domain_from_omniauth = request.env['omniauth.auth'][:info][:email]
+    domain = domain_from_omniauth.split('@')[1]
+    if allowed_domains.include? domain
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+    else
+      @error = true
+      render 'home/index'
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => 'callbacks' }
   
   # Home
-  root       to: 'home#index'
-  get   "/", to: 'home#index', as: 'home'
+  get   '/',                to: 'home#index',               as: 'home'
   
   # Images
   resources :images
-  get "/my",                to: "images#my"
-  get "/add_from_bookmark", to: "images#add_from_bookmark"
-  get "/tags/:tag",         to: "images#index",             as: 'tag'
+  get '/my',                to: 'images#my',                as: 'my'
+  get '/add_from_bookmark', to: 'images#add_from_bookmark', as: 'add_from_bookmark'
+  get '/tags/:tag',         to: 'images#index',             as: 'tag'
   
   # Bookmarklet
-  get "tools",          to: "statics#tools",          as: 'tools'
-  get "bookmarklet",    to: "statics#bookmarklet",    as: 'bookmarklet'
-  get "invalid_image",  to: "statics#invalid_image",  as: 'invalid_image'
+  get 'tools',              to: 'statics#tools',            as: 'tools'
+  get 'bookmarklet',        to: 'statics#bookmarklet',      as: 'bookmarklet'
+  get 'invalid_image',      to: 'statics#invalid_image',    as: 'invalid_image'
+  
+  # Root
+  root to: 'home#index'
   
 end
